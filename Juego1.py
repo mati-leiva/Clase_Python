b2e97901517450599034b85a1a4fa0d3112b7152
mati_leiva <m.leiva@05@ufromail.cl>
2024-10-04 09:29:08 -0300

    AAA!!

--- Juego1.py
+++ Juego1.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  4 08:45:33 2024
-
-@author: icmat
-"""
-
 import pygame
 import sys
 import pygame.locals as pl
@@ -20,19 +13,21 @@ def iniciar_rectangulo():
             pygame.display.update()
 
 
-# Iniciamos los colores
+# Starting the colors
 BLACK = (10,   10,   10)
 WHITE = (240, 240, 240)
 RED = (255,   10,   10)
 GREEN = (10, 255,   10)
 BLUE = (10,   10, 255)
-# Mostramos un display
+# Seting up the display
 DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Drawing')
 DISPLAYSURF.fill(WHITE)
-# Mostramos una colecciòn de puntos
+# We display a colection of points  
+
 for i in range(0, 40):
-    pygame.draw.rect(DISPLAYSURF, RED, (250 + 40 *math.cos(i), 200 + 40*math.sin(i), 2, 2))
-    
+    pygame.draw.rect(DISPLAYSURF, RED, (250 + 40 *
+                     math.cos(i), 200 + 40*math.sin(i), 2, 2))
+
 pygame.display.set_caption("hello World")
 iniciar_rectangulo()
