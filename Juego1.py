43b5f0b80c299f8c8426a56f53e659640151a686
Matias Leiva <m.leiva05@ufromail.cl>
2024-10-04 14:55:21 -0300

    Adding point movemeqnt to the player

--- Juego1.py
+++ Juego1.py
@@ -7,29 +7,43 @@ class Point:
     def __init__(self, pos_x, pos_y):
         self.pos_x = pos_x
         self.pos_y = pos_y
-        [pos_x_mouse,pos_y_mouse]=pygame.mouse.get_pos()    
-        self.vel_x = (pos_x_mouse - self.pos_x)
-        self.vel_y = (pos_y_mouse - self.pos_y)
-        Point.normalice_vel(self)
-        Point.update_position(self)
+        self.vel_x = 0.1
+        self.vel_y = 0.1
 
     def draw_point(self):
-        pygame.draw.rect(DISPLAYSURF, RED, (self.pos_x ,self.pos_y,2,2))
+        pygame.draw.rect(DISPLAYSURF, RED, (self.pos_x ,self.pos_y,10,10))
 
-    def normalice_vel(self):
-        vel = np.array([self.vel_x, self.vel_y])
+
+    def direction_to_mouse(self):
+        [pos_x_mouse,pos_y_mouse]=pygame.mouse.get_pos()    
+        dir_x = (pos_x_mouse - self.pos_x) 
+        dir_y = (pos_y_mouse - self.pos_y) 
         
-
+        vec_pos = np.array([dir_x, dir_y])
+        vel = np.array(vec_pos)/((vec_pos**2).sum()**0.5)
+        
+        self.vel_x += vel[0]*1.1
+        self.vel_y += vel[1]*1.1
+    
+    def velocity_limit(self):
+        if np.linalg.norm([self.vel_x, self.vel_y])>=5.:
+            self.vel_x = self.vel_x * 0.8
+            self.vel_y = self.vel_y * 0.8
 
     def update_position(self):
         self.pos_x += self.vel_x
         self.pos_y += self.vel_y
 
 def display_game():
+            # We display a colection of points
+    Point_1 = Point(200,200)
     while True:
-        Point_1 = Point(201,201)
-        Point_1.draw_point()
 
+        DISPLAYSURF.fill(WHITE)  
+        Point_1.draw_point()
+        Point_1.direction_to_mouse()
+        Point_1.velocity_limit()
+        Point_1.update_position()
         for event in pygame.event.get():
             if event.type == pl.QUIT:
                 pygame.quit()
@@ -49,10 +63,7 @@ BLUE = (10,   10, 255)
 
 DISPLAYSURF = pygame.display.set_mode((400, 400), 0, 32)
 pygame.display.set_caption('Drawing')
-clock = pygame.time.Clock()
-DISPLAYSURF.fill(WHITE)
-# We display a colection of points
-  
+clock = pygame.time.Clock()  
 pygame.display.set_caption("hello World")
 
 display_game()
